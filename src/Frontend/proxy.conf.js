5581935e90ab08c1072b4892a24573adf5da6658
Amos Zürcher <amos.zuercher@gmail.com>
2024-09-22 10:45:58 +0200

    Documentation update

--- src/Frontend/proxy.conf.js
+++ src/Frontend/proxy.conf.js
@@ -1,8 +1,8 @@
 module.exports = {
   "/api": {
     target:
-      process.env["services__weatherapi__https__0"] ||
-      process.env["services__weatherapi__http__0"],
+      process.env["services__messagesapi__https__0"] ||
+      process.env["services__messagesapi__http__0"],
     secure: process.env["NODE_ENV"] !== "development",
     pathRewrite: {
       "^/api": "",
