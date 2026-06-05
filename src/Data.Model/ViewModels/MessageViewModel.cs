472cf28db952d9c203172b070c28770ea9ec297f
zuercheram <zuercheram@users.noreply.github.com>
2026-06-05 19:53:28 +0200

    Moved to Codeberg

--- src/Data.Model/ViewModels/MessageViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Data.Model.ViewModels;
-public class MessageViewModel
-{
-    public int Id { get; set; }
-    public string Message { get; set; }
-    public DateTime Created { get; set; }
-    public DateTime Received { get; set; }
-}
